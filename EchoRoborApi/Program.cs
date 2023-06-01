12b46370c7f4fe76dd9e76437cd6af54acfcfa8b
Pablo <pabloito123321@gmail.com>
2023-05-31 20:50:29 -0600

    agregamos los cors para usar por localhost

--- EchoRoborApi/Program.cs
+++ EchoRoborApi/Program.cs
@@ -2,16 +2,28 @@ using EchoRoborApi.Models;
 using EchoRoborApi.Services;
 using EchoRoborApi.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string _MyCors = "MyCors";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: _MyCors, builder =>
+    {
+        //permite todos los origenes
+        //builder.AllowAnyOrigin();
 
+        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<EchoRobotContext>(options =>
 {
@@ -36,6 +48,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(_MyCors);
+
 
 app.UseAuthorization();
 
